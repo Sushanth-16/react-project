3714ccc6dca8a2925d628b25334a9a0b9a162161
hrushikesh.9 <120154988+hrushikesh2209@users.noreply.github.com>
2026-02-26 12:01:31 +0530

    Render Home at root and add nav links (fix Vercel root showing only H1)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Reg from "./Reg"
 import Home from "./Home"
+
 function App() {
   return (
-    <>
-      <h1>Iam App</h1>
-      <BrowserRouter>
-      <Routes>
-          <Route path="/reg" element={<Reg/>}/>
-          <Route path="/h" element={<Home/>}/>
-      </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <div style={{ padding: 16 }}>
+        <nav style={{ marginBottom: 16 }}>
+          <Link to="/" style={{ marginRight: 12 }}>Home</Link>
+          <Link to="/reg">Register</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/reg" element={<Reg/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
+
 export default App
